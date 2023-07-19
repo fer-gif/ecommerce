36126ac4f006772dbb1beaed313454e070e31ac3
Fermin Hernando <ferminhernando00@gmail.com>
2023-07-19 16:42:58 -0300

    subo php

---
+++ php-javascript/model/conexion.php
@@ -0,0 +1,32 @@
+<?php
+class Conexion
+{
+    private $host = "localhost";
+    private $db = "db_eccomerce";
+    private $user = "root";
+    private $pass = "";
+    private $conexion;
+
+    public function __construct()
+    {
+        try {
+            $this->conexion = new PDO("mysql:host=" . $this->host . "; dbname=" . $this->db . "; charset=utf8", $this->user, $this->pass);
+        } catch (PDOException $e) {
+            throw new Exception($e);
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->conexion;
+    }
+
+    public function dissconect()
+    {
+        $this->conexion = null;
+    }
+}
+
+
+?>  
+
---
+++ php-javascript/model/productos.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'model/conexion.php';
+class EquipoModel
+{
+    private $connection;
+
+    public function __construct()
+    {
+        try {
+            $this->connection = new Conexion();
+        } catch (PDOException $e) {
+            throw new Exception($e);
+        }
+    }
+
+    public function getProductos()
+    {
+        $conexion = $this->connection->getConnection();
+
+        $sentence = $conexion->prepare("SELECT * FROM productos");
+        $sentence->execute();
+        $sentence->setFetchMode(PDO::FETCH_OBJ);
+        $productos = $sentence->fetchAll();
+        return $productos;
+    }
+
+
+    public function getProducto($id){
+        $conexion = $this->connection->getConnection();
+
+        $sentence = $conexion->prepare("SELECT * FROM productos
+                                        WHERE id_productos=?
+                                        ");
+         $sentence->execute(array($id));
+         $producto = $sentence->fetch(PDO::FETCH_ASSOC);
+         return $producto;
+    }
+
+}
